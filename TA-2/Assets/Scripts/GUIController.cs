44b538f6d1c3efe3869cb68a683a90ae8b099192
Bestama Abhi Priambada <bestama.abhi@windowslive.com>
2015-03-11 13:22:22 +0700

    -improved GUI -add multi target scene (blank)

--- TA-2/Assets/Scripts/GUIController.cs
+++ TA-2/Assets/Scripts/GUIController.cs
@@ -15,19 +15,30 @@ public class GUIController : MonoBehaviour {
 
     void OnGUI()
     {
+        float width = Screen.width;
+        float height = Screen.height;
+        float buttonWidth = width / 2;
+        float buttonHeight = height / 4;
+        float topPosition = 100;
+
         // Make a background box
-        GUI.Box(new Rect(10, 10, 150, 90), "Choose Methods");
+        GUI.Box(new Rect(50, 50, width - 100, height - 100), "Choose Methods");
 
         // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-        if (GUI.Button(new Rect(30, 40, 100, 20), "Image Target"))
+        if (GUI.Button(new Rect(width / 4, topPosition, buttonWidth, buttonHeight), "Image Target"))
         {
             Application.LoadLevel(1);
         }
-
+        topPosition = topPosition + buttonHeight;
         // Make the second button.
-        if (GUI.Button(new Rect(30, 70, 100, 20), "Cylinder Target"))
+        if (GUI.Button(new Rect(width / 4, topPosition, buttonWidth, buttonHeight), "Cylinder Target"))
         {
             Application.LoadLevel(2);
+        }
+        topPosition = topPosition + buttonHeight;
+        if (GUI.Button(new Rect(width / 4, topPosition, buttonWidth, buttonHeight), "Multi Target"))
+        {
+            //Application.LoadLevel(2);
         }
     }
 }
